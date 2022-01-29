9307673f17c71b3443c7723b1ffaf6beafcbb78a
ankitpal1029 <ankitkumarpal27@gmail.com>
2022-01-29 20:54:29 +0530

    hard coded form for transaction

--- client/src/components/Welcome.tsx
+++ client/src/components/Welcome.tsx
@@ -7,8 +7,37 @@ import { Loader } from "./";
 const commonStyles =
   "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
+const Input = ({
+  placeholder,
+  name,
+  type,
+  value,
+  handleChange,
+}: {
+  placeholder: string;
+  name: string;
+  type: string;
+  value: string;
+  handleChange: Function;
+}) => {
+  return (
+    <input
+      placeholder={placeholder}
+      type={type}
+      step="0.0001"
+      value={value}
+      onChange={(e) => handleChange(e, name)}
+      className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+    />
+  );
+};
+
 const Welcome = () => {
   const connectWallet = () => {};
+
+  const handleChange = (e: HTMLInputElement, name: string) => {};
+
+  const handleSubmit = () => {};
   return (
     <div className="flex w-full justify-center items-center">
       <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
@@ -50,6 +79,49 @@ const Welcome = () => {
                 <p className="text-white font-semibold text-sm">Ethereum</p>
               </div>
             </div>
+          </div>
+          <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
+            <Input
+              placeholder="Address To:"
+              name="addressTo"
+              type="text"
+              value=""
+              handleChange={handleChange}
+            />
+            <Input
+              placeholder="Amount(ETH)"
+              name="amount"
+              type="number"
+              value=""
+              handleChange={handleChange}
+            />
+            <Input
+              placeholder="Keyword (GIF)"
+              name="keyword"
+              type="text"
+              value=""
+              handleChange={handleChange}
+            />
+            <Input
+              placeholder="Enter Message"
+              name="message"
+              type="text"
+              value=""
+              handleChange={handleChange}
+            />
+            <div className="h-[1px] w-full bg-gray-400 my-2" />
+
+            {false ? (
+              <Loader />
+            ) : (
+              <button
+                type="button"
+                onClick={handleSubmit}
+                className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer"
+              >
+                Send Now
+              </button>
+            )}
           </div>
         </div>
       </div>
